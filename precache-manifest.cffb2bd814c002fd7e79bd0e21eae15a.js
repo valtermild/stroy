e3fc810de4425bf6527a2f7d0304017cb6bbdaab
unknown <valtermild@gmail.com>
2019-08-06 16:58:39 +0300

    Updates

---
+++ precache-manifest.cffb2bd814c002fd7e79bd0e21eae15a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dde6aba4713ea2daa85c7a60c8baad9",
+    "url": "/stroycity/index.html"
+  },
+  {
+    "revision": "f7121ef4a8e43a41aa93",
+    "url": "/stroycity/static/css/2.4b5295fb.chunk.css"
+  },
+  {
+    "revision": "e16e65cd654e5a8b86a1",
+    "url": "/stroycity/static/css/main.52b28e15.chunk.css"
+  },
+  {
+    "revision": "f7121ef4a8e43a41aa93",
+    "url": "/stroycity/static/js/2.82414fa4.chunk.js"
+  },
+  {
+    "revision": "e16e65cd654e5a8b86a1",
+    "url": "/stroycity/static/js/main.ca7f57c3.chunk.js"
+  },
+  {
+    "revision": "643382b747670658fb10",
+    "url": "/stroycity/static/js/runtime~main.c56fe692.js"
+  },
+  {
+    "revision": "4fe6f9caff8b287170d51d3d71d5e5c6",
+    "url": "/stroycity/static/media/lg.4fe6f9ca.ttf"
+  },
+  {
+    "revision": "5fd4c338c1a1b1eeeb2c7b0a0967773d",
+    "url": "/stroycity/static/media/lg.5fd4c338.woff"
+  },
+  {
+    "revision": "c066c5448562b3ccaefb6408ce4b4ae1",
+    "url": "/stroycity/static/media/lg.c066c544.svg"
+  },
+  {
+    "revision": "ecff11700aad0000cf3503f537d1df17",
+    "url": "/stroycity/static/media/lg.ecff1170.eot"
+  }
+]);
